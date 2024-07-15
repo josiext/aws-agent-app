2d1308c5f2640a04b0ff16df07afbfa6bbeefb0f
José Núñez <79015124+josiext@users.noreply.github.com>
2024-07-15 16:13:02 -0400

    updates

---
+++ src/actions/invoque-agent.ts
@@ -0,0 +1,67 @@
+"use server";
+
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  BedrockAgentRuntimeClient,
+  InvokeAgentCommand,
+} from "@aws-sdk/client-bedrock-agent-runtime";
+
+/**
+ * @typedef {Object} ResponseBody
+ * @property {string} completion
+ */
+
+/**
+ * Invokes a Bedrock agent to run an inference using the input
+ * provided in the request body.
+ *
+ * @param {string} prompt - The prompt that you want the Agent to complete.
+ * @param {string} sessionId - An arbitrary identifier for the session.
+ */
+export const invokeBedrockAgent = async (formData: FormData) => {
+  const prompt = formData.get("prompt");
+  const sessionId = formData.get("session");
+
+  console.log("aqui", prompt);
+
+  /*   const client = new BedrockAgentRuntimeClient({ region: "us-east-1" });
+   */ // const client = new BedrockAgentRuntimeClient({
+  //   region: "us-east-1",
+  //   credentials: {
+  //     accessKeyId: "accessKeyId", // permission to invoke agent
+  //     secretAccessKey: "accessKeySecret",
+  //   },
+  // });
+  /* 
+  const agentId = "AJBHXXILZN";
+  const agentAliasId = "AVKP1ITZAA";
+
+  const command = new InvokeAgentCommand({
+    agentId,
+    agentAliasId,
+    sessionId,
+    inputText: prompt,
+  });
+
+  try {
+    let completion = "";
+    const response = await client.send(command);
+
+    if (response.completion === undefined) {
+      throw new Error("Completion is undefined");
+    }
+
+    for await (let chunkEvent of response.completion) {
+      const chunk = chunkEvent.chunk;
+      console.log(chunk);
+      const decodedResponse = new TextDecoder("utf-8").decode(chunk.bytes);
+      completion += decodedResponse;
+    }
+
+    return { sessionId: sessionId, completion };
+  } catch (err) {
+    console.error(err);
+  } */
+};
