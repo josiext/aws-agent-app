6e48271010efb1b87666deba9a4497cf38b1e824
José Núñez <79015124+josiext@users.noreply.github.com>
2024-07-15 16:47:59 -0400

    invoque agent

--- src/actions/invoque-agent.ts
+++ src/actions/invoque-agent.ts
@@ -1,42 +1,28 @@
 "use server";
 
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 import {
   BedrockAgentRuntimeClient,
   InvokeAgentCommand,
 } from "@aws-sdk/client-bedrock-agent-runtime";
 
-/**
- * @typedef {Object} ResponseBody
- * @property {string} completion
- */
-
-/**
- * Invokes a Bedrock agent to run an inference using the input
- * provided in the request body.
- *
- * @param {string} prompt - The prompt that you want the Agent to complete.
- * @param {string} sessionId - An arbitrary identifier for the session.
- */
 export const invokeBedrockAgent = async (formData: FormData) => {
   const prompt = formData.get("prompt");
-  const sessionId = formData.get("session");
+  /*   const sessionId = formData.get("session");
+   */
 
+  const sessionId = "session-1";
   console.log("aqui", prompt);
 
-  /*   const client = new BedrockAgentRuntimeClient({ region: "us-east-1" });
-   */ // const client = new BedrockAgentRuntimeClient({
-  //   region: "us-east-1",
-  //   credentials: {
-  //     accessKeyId: "accessKeyId", // permission to invoke agent
-  //     secretAccessKey: "accessKeySecret",
-  //   },
-  // });
-  /* 
-  const agentId = "AJBHXXILZN";
-  const agentAliasId = "AVKP1ITZAA";
+  const client = new BedrockAgentRuntimeClient({
+    region: process.env.AGENT_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const agentId = process.env.AGENT_ID;
+  const agentAliasId = process.env.AGENT_ALIAS;
 
   const command = new InvokeAgentCommand({
     agentId,
@@ -60,8 +46,10 @@ export const invokeBedrockAgent = async (formData: FormData) => {
       completion += decodedResponse;
     }
 
+    console.log("Completion: ", completion);
+
     return { sessionId: sessionId, completion };
   } catch (err) {
     console.error(err);
-  } */
+  }
 };
